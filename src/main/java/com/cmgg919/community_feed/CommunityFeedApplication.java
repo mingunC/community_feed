2b398b91e0d57ad97fc9ffa0db4cfdb9aced75ab
mingunC <mingun.ryan.choi@gmail.com>
2024-12-02 11:31:45 -0500

    git init

---
+++ src/main/java/com/cmgg919/community_feed/CommunityFeedApplication.java
@@ -0,0 +1,13 @@
+package com.cmgg919.community_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmgg919/community_feed/CommunityFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmgg919.community_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
